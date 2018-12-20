ab1db8c89fa0f692c1763d4486802e2931c19707
dlguddn3214 <dlguddn3214@naver.com>
2018-12-21 00:26:15 +0900

    node

---
+++ skplanet/nodejs/event.js
@@ -0,0 +1,12 @@
+process.on('exit', function (code) {
+    console.log('exit event : ', code)
+});
+
+process.once('exit', function (code) {
+    console.log('exit once:', code)
+})
+
+process.emit('exit');
+process.emit('exit', 0);
+process.emit('exit', 1);
+
---
+++ skplanet/nodejs/util.js
@@ -0,0 +1,8 @@
+var util = require('util');
+
+var str1 = util.format('%d + %d = %d', 1, 2, (1 + 2))
+
+console.log(str1)
+
+var str2 = util.format('%s %s', 'hello', 'world')
+console.log(str2)
