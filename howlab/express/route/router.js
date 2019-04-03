d398a37954e321bbf091d92f3a7dfc213c229019
dlguddn3214 <dlguddn3214@naver.com>
2019-04-04 01:25:05 +0900

    bodyParser

--- howlab/express/route/router.js
+++ howlab/express/route/router.js
@@ -8,4 +8,27 @@ router.route('/user')
     .put(auth.isBasicAuthenticated, user.updateUser)
     .delete(auth.isBasicAuthenticated, user.deleteUser)
 
+router.route('/test')
+    .get((req, res) => {
+        //보안상 비추
+        console.log('도착')
+        console.log(req.query)
+        res.send('확인')
+    })
+    .post((req, res) => {
+        console.log(req.body)
+        res.send('post방식')
+    })
+
+
+router.route('/test/:id')
+    .get((req, res) => {
+        //데이터를 업데이트,삭제
+        console.log('도착2')
+        console.log(req.params)
+        res.send('확인2')
+    })
+
+
+
 module.exports = router;
--- howlab/express/server.js
+++ howlab/express/server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const router = require('./route/router');
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded  
+//false를주면 String Object로 받는다.
+//true를 주면 어떤타입이든 다 받는다.
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.use(router)
 
